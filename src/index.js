94a9ab96d1e5996379b33b25d66d7e1ee9325f04
vydoorya <ovydoorya@gmail.com>
2024-08-20 19:57:50 +0530

    version 5

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import express from 'express'
 import serverConfig from './config/serverConfig.js'
 import mongodbConnection from './config/mongodbConfig.js'
 import apiRouter from "./routes/index.js"
+import cors from "cors"
 
 const app = express()
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 app.use(express.json());
 app.use("/api",apiRouter)
 
